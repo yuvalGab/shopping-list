a8bdf2e98fc4c45495a3c6da4b43e9ac02c04bd8
Yuval Gabian <yuvalgab2007@gmail.com>
2018-03-01 09:32:08 +0200

    create user with hash password

--- server/routers/user.js
+++ server/routers/user.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
-router.post('/register', (req, res) => {
-  res.send(true)
+const saltRounds = 10
+
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const salt = await bcrypt.genSalt()
+    const hash = await bcrypt.hash(password, salt)
+    req.body.password = hash
+	  await User.create(req.body)
+  } catch (err) {
+    return res.sendStatus(500)
+  }
+
+  res.sendStatus(201)
 })
 
 router.post('/login', (req, res) => {
